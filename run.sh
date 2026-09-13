367532260f91dea0ab7386be954533d4956e3a1b
Arian Baishya <arian.baishya2004@gmail.com>
2026-09-13 14:04:18 -0500

    update run script

--- run.sh
+++ run.sh
@@ -5,7 +5,15 @@ source ./.env
 mkdir -p "./${CONTAINER_ROOT_DATA_DIR:?}"/{home,root,etc,log,usr,var}
 mkdir -p ./data
 
-xhost +SI:localuser:$USER # allows your current local user to connect to the X server
+xhost "+SI:localuser:$USER" # allows your current local user to connect to the X server
 
 # runs the container (building if needed). deletes container on exit and starts it with interactive TTY
-sudo docker compose run --build --rm -it ros2
+docker_command='docker compose run --build --rm -it ros2'
+
+# checks if docker socket is writable as a roundabout way to check if we need to use `sudo`
+docker_socket_path='/var/run/docker.sock'
+if test -w "$docker_socket_path"; then
+	$docker_command
+else
+	sudo $docker_command
+fi
